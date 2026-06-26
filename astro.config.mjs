af98de43acce439a263e36827fa4a49e716bc702
cancerlogo <cancer2811344635@gmail.com>
2026-06-26 21:14:35 +0800

    Update site name to cancerlogo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://xxx.pages.dev',
+  site: 'https://cancerlogo.pages.dev',
   vite: {
     plugins: [tailwindcss()],
   },
